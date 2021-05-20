533243e2170f5a42de79bca9a814ef8045753eab
Fernando <fernandoamz148@gmail.com>
2021-05-19 23:54:23 -0500

    remove comments

--- app.js
+++ app.js
@@ -14,18 +14,15 @@ const auth = require('./routes/auth');
 
 const app = express();
 
-// view engine setup
 const csrfMiddleware = csrf({
   cookie: {
     httpOnly: true,
     maxAge: 3600,
   }
 });
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,11 +34,9 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/auth', auth);
 
-//mysql connection pool
 const sqlPool = require('./modules/sqlPool');
 global.sqlPool = sqlPool;
 
-//mssql connection
 const {
   $MYSQL_ROOT_USER,
   $MYSQL_ROOT_PASSWORD,
@@ -55,11 +50,11 @@ global.conn = {
 	server: $MYSQL_HOST,
 	database: $MYSQL_DATABASE,
 	options: {
-		encrypt: true // If you are using Microsoft Azure SQL Database, keep this.
+		encrypt: true 
 	}
 };
 
-//jwt configuration
+
 global.jwtSecret = 'secret';
 global.jwtExpiresIn = 15 * 24 * 3600;
 
@@ -71,20 +66,18 @@ app.use(function(req, res, next) {
   next();
 });
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  
   res.status(err.status || 500);
   res.render('error');
 });
